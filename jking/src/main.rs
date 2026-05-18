a6ab9641a83e4cfc3391a2308da6ab81c007ea45
Jako Keskküla <44132378+P3-J@users.noreply.github.com>
2026-05-18 20:55:22 +0300

    collisions not working? ball spawn and trenchbroom

--- jking/src/main.rs
+++ jking/src/main.rs
@@ -1,5 +1,7 @@
+use avian3d::prelude::*;
 use bevy::{
     color::palettes::basic::BLUE,
+    ecs::relationship::RelationshipSourceCollection,
     input::keyboard::{Key, KeyboardInput},
     prelude::*,
 };
@@ -8,6 +10,9 @@ use bevy::window::{
     CursorGrabMode, CursorIcon, CursorOptions, PresentMode, SystemCursorIcon, WindowLevel,
     WindowTheme,
 };
+use bevy_flycam::prelude::*;
+use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
+use bevy_trenchbroom::{anyhow, class::QuakeClassSpawnView, prelude::*};
 
 use crate::{
     food::{Food, FoodPlugin},
@@ -23,6 +28,7 @@ impl Plugin for LaunchPlugin {
         app.add_systems(Startup, spawn_triangle)
             .insert_resource(GreetTimer(Timer::from_seconds(0.1, TimerMode::Once)))
             .insert_resource(WorldSize { x: 500.0, y: 500.0 })
+            .register_type::<SpawnType>()
             .add_systems(Update, move_player)
             .add_systems(Update, (rotate).chain());
     }
@@ -43,15 +49,24 @@ fn main() {
                     maximize: false,
                     ..Default::default()
                 },
-                resizable: false,
+                resizable: true,
                 visible: true,
                 ..default()
             }),
             ..default()
         }))
+        .add_plugins(TrenchBroomPlugins(
+            TrenchBroomConfig::new("catNapper")
+                .default_solid_scene_hooks(|| SceneHooks::new().convex_collider()),
+        ))
         .add_plugins(FoodPlugin)
         .add_plugins(LaunchPlugin)
         .add_plugins(ScoreboardPlugin)
+        .add_plugins(EguiPlugin::default())
+        .add_plugins(PhysicsPlugins::default())
+        .add_plugins(WorldInspectorPlugin::new())
+        .add_plugins(PlayerPlugin)
+        .add_systems(Update, ball_query)
         .run();
 }
 
@@ -59,8 +74,13 @@ fn spawn_triangle(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut mats: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
-    commands.spawn((Camera2d, Transform::default()));
+    //commands.spawn((Camera2d, Transform::default()));
+    /*  commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Dir3::Y),
+    )); */
 
     let a = meshes.add(Rectangle::new(62.5, 62.5));
 
@@ -69,6 +89,24 @@ fn spawn_triangle(
         MeshMaterial2d(mats.add(Color::from(BLUE))),
         Transform::default(),
     ));
+
+    /*   commands.insert_resource(GlobalAmbientLight {
+        color: BLUE.into(),
+        brightness: 200.0,
+        ..default()
+    }); */
+
+    commands.spawn((
+        PointLight {
+            intensity: 100_000.0,
+            color: BLUE.into(),
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(0.0, 5.0, 0.0),
+    ));
+
+    commands.spawn(SceneRoot(asset_server.load("maps/test.map#Scene")));
 }
 
 fn rotate(
@@ -105,6 +143,64 @@ fn move_player(
     }
     if key.pressed(KeyCode::KeyS) {
         query.translation.y -= 300.0 * time.delta_secs();
+    }
+}
+
+#[point_class(
+    hooks(SceneHooks::new().push(Self::trying_to_spawn_ball))
+)]
+struct BallSpawnPoint;
+impl BallSpawnPoint {
+    pub fn trying_to_spawn_ball(view: &mut QuakeClassSpawnView) -> anyhow::Result<()> {
+        let transform = *view.world.get::<Transform>(view.entity).unwrap();
+
+        view.world.commands().spawn((
+            SpawnType {
+                sp_enum: SpawnShape::Ball,
+            },
+            transform,
+        ));
+
+        Ok(())
+    }
+}
+
+#[derive(Component, Reflect)]
+#[reflect(Component)]
+struct SpawnType {
+    sp_enum: SpawnShape,
+}
+
+#[derive(Reflect)]
+enum SpawnShape {
+    Ball,
+    Square,
+}
+
+fn ball_query(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    query: Query<(Entity, &SpawnType, &Transform), Added<SpawnType>>,
+) {
+    for (entity, spawn_type, transform) in &query {
+        println!("got 1");
+        let mesh = match spawn_type.sp_enum {
+            SpawnShape::Ball => meshes.add(Sphere::new(1.0)),
+            SpawnShape::Square => meshes.add(Cuboid::from_length(2.0)),
+        };
+
+        commands.entity(entity).insert((
+            Mesh3d(mesh),
+            RigidBody::Dynamic,
+            Collider::sphere(1.0),
+            MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+            GlobalTransform::from_xyz(
+                transform.translation.x,
+                transform.translation.y,
+                transform.translation.z,
+            ),
+        ));
     }
 }
 
